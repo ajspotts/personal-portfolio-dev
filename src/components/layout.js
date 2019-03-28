6a19ea43f467f9ae9e205514bb07250675cb3b54
Alexander <ajspottswood@gmail.com>
2019-03-28 19:34:19 -0400

    removed github pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,6 +27,7 @@ const Layout = ({ children }) => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
         <Menu />
+        <div>NETLIFY TEST</div>
         <div
           style={{
             margin: `0 auto`,
