b4eec28c7e958ead12fba09fd0c21d5e393e6374
Alexander <ajspottswood@gmail.com>
2019-05-03 19:40:01 -0400

    altered bootstrap import in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
 import "../styles/layout.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 const Layout = ({ children }) => (
   <StaticQuery
